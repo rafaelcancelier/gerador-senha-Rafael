9036b06324832569edbfdc508e503fa341d41dbc
rafaelcancelier <145573093+rafaelcancelier@users.noreply.github.com>
2025-09-09 10:28:31 -0300

    1234590-

--- main.js
+++ main.js
@@ -67,6 +67,11 @@ function geraSenha() {
 function classificaSenha(){
     forcaSenha.classList.remove('fraca','media','forte');
     if (tamanhoSenha > 11){
-        forcaSenha.classList.add('forte');
+    forcaSenha.classList.add('forte');
+    }else if (tamanhoSenha > 5 && tamanhoSenha <12) {
+            forcaSenha.classList.add('media');
+    }else if (tamanhoSenha <= 5) {
+        forcaSenha.classList.add('fraca');
     }
-}
+    }
+
